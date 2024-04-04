4ee176b2584fff8991cb520a2a0eb78492220dd4
heriquemtss <henriquegomesdematos08@gmail.com>
2024-04-04 11:19:32 -0300

    inserindo splash na tela de bem vindo ao sistema

---
+++ view/bemvindo.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/splash.css">
+    <script src="../assets/javascript/tab.js" type="text/javascript"></script>
+    <title>Bem vindo</title>
+</head>
+<body>
+<div class="splash">
+        <div class="intro">
+            <img src="../assets/img/logo 1.svg" style="width: 12%;" alt="logo">
+            <h1 class="logo">
+                <span class="logo-parts">I</span>
+                <span class="logo-parts">N</span>
+                <span class="logo-parts">I</span>
+                <span class="logo-parts">C</span>
+                <span class="logo-parts">I</span>
+                <span class="logo-parts">A</span>
+                <span class="logo-parts">N</span>
+                <span class="logo-parts">D</span>
+                <span class="logo-parts">O</span>
+            </h1>
+        </div>
+    </div>
+    <script src="../assets/javascript/splash.js"></script>
+
+    <p>Entrou no sistema</p>
+</body>
+</html>
