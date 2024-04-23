65c2a70feafe55c15dd302a3e4a38a2057ff5c94
heriquemtss <henriquegomesdematos08@gmail.com>
2024-04-23 12:45:23 -0300

    inserindo abas na pagina plantia.php - inserindo inteligencia artificial na aba Inteligência Artificial

--- view/plantia.php
+++ view/plantia.php
@@ -21,6 +21,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/plantia.css">
     <link rel="stylesheet" href="../assets/css/splash.css">
+    <link rel="stylesheet" href="../assets/css/tab.css">
     <link rel="shortcut icon" href="../assets/img/logo 1.svg" type="image/x-icon">
 </head>
 <body>
@@ -43,16 +44,66 @@
         </div>
     <script src="../assets/javascript/splash.js"></script>
     
-<div class="cabecalho">
-    <img id="logo" src="../assets/img/logo 1.svg" alt="logo do site" >
-    <div class="usuario-sair">
-        <?php 
-            echo "<p class='usuario'><u>$logado</u></p>";
-        ?>
-        <a class="btn btn-outline btn-sm" href="login.php">Sair</a>
-    </div>
-</div>
 
+    <div class="cabecalho">
+        <img id="logo" src="../assets/img/logo 1.svg" alt="logo do site" >
+            <div class="usuario-sair">
+                <?php 
+                    echo "<p class='usuario'><u>$logado</u></p>";
+                ?>
+                <a class="btn btn-outline btn-sm" href="login.php">Sair</a>
+            </div>
+    </div>
+
+    <div class="container">
+
+    </div>
+
+    <!------------Tabelas--------------->
+    <div id="container-tab">
+        <div class="tab-buttons">
+            <button class="tab-btn active" content-id="ia">
+                Inteligência Artificial
+            </button>
+
+            <button class="tab-btn" content-id="cadastro-planta">
+                Cadastro da planta
+            </button>
+
+            
+        </div>
+
+        <div class="tab-contents">
+            <div class="content" id="ia">
+                
+                <div class="infos">
+                    <h1 class="content-title">
+                        
+                    </h1>
+                    <iframe class="iaframe" src="https://www.blackbox.ai/agent/VerdeVivo2PPaGv8c" width="1000" height="500" allowfullscreen frameborder="0" scrolling="no"></iframe>
+                    <p class="content-description">
+                       
+                    </p>
+                </div>
+            </div>
+
+            <div class="content" id="cadastro-planta">
+
+                <div class="infos">
+                    <h1 class="content-title">
+                        Services
+                    </h1>
+
+                    <p class="content-description">
+                       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, repellat! Id ab similique velit, molestiae, aliquam illum officiis sit totam rem voluptas magnam, atque dicta magni perferendis recusandae reiciendis necessitatibus! 
+                    </p>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <!--------------API de Libras--------------------->
     <div vw class="enabled">
         <div vw-access-button class="active"></div>
             <div vw-plugin-wrapper>
@@ -62,10 +113,13 @@
   
     <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
     <script> new window.VLibras.Widget('https://vlibras.gov.br/app');</script>
-<!--
+
+    <script src="../assets/javascript/tab.js"></script>
+
+
 <footer>
     <p>© 2024 Verde Vivo. Todos os direitos reservados.</p>
 </footer>
--->
+
 </body>
 </html>
