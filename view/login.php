ef1047c266ca8759cf2218d4f8d51354f668f203
heriquemtss <henriquegomesdematos08@gmail.com>
2024-04-24 15:46:41 -0300

    inserindo api de libras em todas as paginas

--- view/login.php
+++ view/login.php
@@ -26,7 +26,6 @@
             </div>
 
             <div class="remember-forgot">
-                <label><input type="checkbox"> Lembrar senha</label>
                 <a href="#">Esqueceu a senha?</a>
             </div>
 
@@ -44,5 +43,16 @@
         </form>
     </div>
     
+    <!--------------API de Libras--------------------->
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+            <div vw-plugin-wrapper>
+                <div class="vw-plugin-top-wrapper"></div>
+            </div>
+    </div>
+  
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script> new window.VLibras.Widget('https://vlibras.gov.br/app');</script>
+
 </body>
 </html>
